ef9f120d770e6a0574903e8018db174f73d3ad83
shinjir0u <htooagwai9@gmail.com>
2026-05-04 05:13:37 +0000

    Separate config files to decouple configurations

---
+++ transaction/src/main/java/com/app/sentinelpay/transaction/config/TransactionConfiguration.java
@@ -0,0 +1,9 @@
+package com.app.sentinelpay.transaction.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class TransactionConfiguration {
+}
